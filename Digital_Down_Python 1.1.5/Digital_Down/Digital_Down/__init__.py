4a119bb9cb680845f41e25de7c7df18eed636ce7
Digital Down <bartbragg@yahoo.com>
2024-09-09 15:27:11 -0500

    fixes

---
+++ Digital_Down_Python 1.1.5/Digital_Down/Digital_Down/__init__.py
@@ -0,0 +1,18 @@
+# Audio
+from .Audio.ExtractionSynthesis import *
+from .Audio.SinglePhaseExtraction import *
+from .Audio.Methylphenethylamine import *
+from .Audio.AudioSampleValues import *
+from .Audio.BetaPhaseExtraction import *
+
+# Code
+
+# Image
+from .Image.ImageProselyte import *
+
+# Text
+from .Text.Documentation import *
+
+# Video
+from .Video.MindHarvestNetwork import *
+
